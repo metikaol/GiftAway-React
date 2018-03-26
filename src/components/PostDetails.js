1d54dbaf3ad916e4ad04d50dc6d8b5cc7f8b963c
Metika Olanriksupak <metika.o@hotmail.com>
2018-03-25 18:24:24 -0700

    Add Jumbotron to PostDetails

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import Field from './Field';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Jumbotron, Container } from 'reactstrap';
 
 
 function PostDetails (props) {
   const {author = {}} = props;
 
   return (
-    <div>
+
+      <div>
+        <Jumbotron>
+          <Container>
+            <h1 className="display-3">{props.title}</h1>
+            <p className="lead">{props.body}</p>
+            Field name="Posted" value={props.created_at} />
+            <p>By {author.first_name}</p>
+          </Container>
+        </Jumbotron>
+      </div>
+      //
+      // <h2>{props.title}</h2>
+      // <p>{props.body}</p>
+      // <p>By {author.first_name}</p>
+      // {/* <Field name="View Count" value={props.view_count} /> */}
+      // <Field name="Posted" value={props.created_at} />
+      // {/* <Field name="Updated At" value={props.updated_at} /> */}
 
 
-      <h2>{props.title}</h2>
-      <p>{props.body}</p>
-      <p>By {author.full_name}</p>
-      <Field name="View Count" value={props.view_count} />
-      <Field name="Created At" value={props.created_at} />
-      <Field name="Updated At" value={props.updated_at} />
-
-    </div>
   );
 }
 
